01d4a5c56fe6d956bc7cc78e924064f8703620b7
Rakeshmallapur <105627827+Rakeshmallapur@users.noreply.github.com>
2022-09-15 15:10:18 +0530

    Delete SenderService.java

--- SenderService.java
+++
@@ -1,9 +0,0 @@
-ackage Sender;
-
-import java.util.List;
-
-public interface SenderService {
-		public List<SenderParameters> receiveReadingsFromBattery(int streamcount);
-		public void sendReadingsToConsole();
-
-}
