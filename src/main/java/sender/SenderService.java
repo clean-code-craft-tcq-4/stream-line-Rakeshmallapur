f29c64a2c37201bea5865b1cad51a49733b29dd9
Rakeshmallapur <105627827+Rakeshmallapur@users.noreply.github.com>
2022-09-15 15:46:21 +0530

    Delete src/main/java/sender directory

--- src/main/java/sender/SenderService.java
+++
@@ -1,9 +0,0 @@
-package sender;
-
-import java.util.List;
-
-public interface SenderService {
-		public List<SenderParameters> receiveReadingsFromBattery(int streamcount);
-		public void sendReadingsToConsole();
-
-}
