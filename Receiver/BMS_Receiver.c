6e0dfa8a776ea333c3200e0a85a787261e829f85
Amit2509 <amitsrivastava2509@gmail.com>
2022-10-26 15:49:51 +0530

    Update BMS_Receiver.c

--- Receiver/BMS_Receiver.c
+++ Receiver/BMS_Receiver.c
@@ -65,3 +65,23 @@ void FindSMA ()
   SMA_SOC = SOC_Sum/NUMBER_OF_SAMPLES_FOR_SMA;
   SMA_ChargeRate = ChargeRate_Sum/NUMBER_OF_SAMPLES_FOR_SMA;
 }
+
+
+void ReadConsolePrintMinMaxSMA()
+{
+  float temperature, SOC, ChargeRate;
+  char title[30];
+  fgets(title,30,stdin);
+  puts(title);
+  for (int i=0; i<50; i++)
+  {
+    ReadFromConsoleInput(&temperature, &SOC, &ChargeRate);
+    FindMinAndMax(temperature, &Min_Temp, &Max_Temp);
+    FindMinAndMax(SOC, &Min_SOC, &Max_SOC);
+    FindMinAndMax(ChargeRate, &Min_ChargeRate, &Max_ChargeRate);
+    FindSMA();
+    printf("Temperature: Min = %f, Max = %f, SMA = %f\n", Min_Temp, Max_Temp, SMA_Temp);
+    printf("SOC: Min = %f, Max = %f, SMA = %f\n", Min_SOC, Max_SOC, SMA_SOC);
+    printf("ChargeRate: Min = %f, Max = %f, SMA = %f\n\n", Min_ChargeRate, Max_ChargeRate, SMA_ChargeRate);
+  }
+}
