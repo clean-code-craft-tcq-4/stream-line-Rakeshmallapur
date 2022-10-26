2bb2772c0d883033aaae56ab7333ae7dce039e0d
Amit2509 <amitsrivastava2509@gmail.com>
2022-10-26 15:16:54 +0530

    Update BMS_Receiver.h

--- Receiver/BMS_Receiver.h
+++ Receiver/BMS_Receiver.h
@@ -1,4 +1,16 @@
 #define NUMBER_OF_SAMPLES_FOR_SMA 5
+extern float Min_Temp;
+extern float Max_Temp;
+extern float SMA_Temp;
+extern float Temp_Array[NUMBER_OF_SAMPLES_FOR_SMA];
+extern float Min_SOC;
+extern float Max_SOC;
+extern float SMA_SOC;
+extern float SOC_Array[NUMBER_OF_SAMPLES_FOR_SMA];
+extern float Min_ChargeRate;
+extern float Max_ChargeRate;
+extern float SMA_ChargeRate;
+extern float ChargeRate_Array[NUMBER_OF_SAMPLES_FOR_SMA];
 
 int ReadFromConsoleInput(float* temperature, float* SOC, float* ChargeRate);
 void FindMinAndMax(float data, float* Min, float* Max);
