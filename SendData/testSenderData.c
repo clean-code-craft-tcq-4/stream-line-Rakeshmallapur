fc1dcc1152c5747c4b7d21cb4f42e14edf7ae4e9
Amit2509 <amitsrivastava2509@gmail.com>
2022-10-27 22:08:52 +0530

    Update testSenderData.c

--- SendData/testSenderData.c
+++ SendData/testSenderData.c
@@ -5,19 +5,30 @@ float Temp[50] = {0};
 float SOC[50] = {0};
 float ChargeRate[50] = {0};
 
-int main()
+void GenerateData()
 {
-  printf("Temperature;SOC;ChargeRate\n");
   for (int i = 0; i < BMS_SENDER_BUFFER_SIZE; i++)
   {
     Temp[i] = i;
     SOC[i] = 1.5*i;
     ChargeRate[i] = (float)i/BMS_SENDER_BUFFER_SIZE;
   }
+}
+
+void SendData()
+{
+  printf("Temperature;SOC;ChargeRate\n");
+
   for (int i = 0; i < BMS_SENDER_BUFFER_SIZE; i++)
   {
     printf("%f;%f;%f\n",Temp[i],SOC[i],ChargeRate[i]);
   }
-  
+}
+
+int main()
+{
+  GenerateData();
+  SendData(); 
+  SendData();
   return 0;
 }
